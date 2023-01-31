f2414791438c50ded298a9d0f58a94d580614b2c
idilblc <116103046+idilblc@users.noreply.github.com>
2023-01-31 16:10:24 +0000

    Update 3 - Topic Modelling
    Added code for preprocessing & topic modelling visualisation

--- source_code.py
+++ source_code.py
@@ -155,7 +155,225 @@ plt.savefig("df_wc_language_pie_chart.png", bbox_inches='tight')
 # Show the pie chart
 plt.show()
 
+# CLEANING TWEETS AND CREATING VISUALISATIONS
+
+# import required packages
+import re
+import numpy as np
+import gensim
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+
+# Removing irrelevant Spam tweets that pollute our dataset.
+sequence = '#sbsworldcup #u2 #shaunswalue #u2worldexclusive #u2australia #swalue #gay'
+df_lgbt = df_lgbt[~df_lgbt['text'].str.contains(sequence)]
+
+# Keeping tweets that are not possibly sensitive because they are sexually explicit
+df_lgbt= df_lgbt.loc[df_lgbt['possibly_sensitive'] != True]
+
+# keeping english tweets only because our focus is on english words
+df_lgbt = df_lgbt.loc[df_lgbt['lang'] == 'en']
+
+# A list of contractions from http://stackoverflow.com/questions/19790188/expanding-english-language-contractions-in-python
+contractions = {
+"ain't": "am not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he's": "he is",
+"how'd": "how did",
+"how'll": "how will",
+"how's": "how is",
+"i'd": "i would",
+"i'll": "i will",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'll": "it will",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"must've": "must have",
+"mustn't": "must not",
+"needn't": "need not",
+"oughtn't": "ought not",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"she'd": "she would",
+"she'll": "she will",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"that'd": "that would",
+"that's": "that is",
+"there'd": "there had",
+"there's": "there is",
+"they'd": "they would",
+"they'll": "they will",
+"they're": "they are",
+"they've": "they have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'll": "we will",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"where'd": "where did",
+"where's": "where is",
+"who'll": "who will",
+"who's": "who is",
+"won't": "will not",
+"wouldn't": "would not",
+"you'd": "you would",
+"you'll": "you will",
+"you're": "you are"
+}
+
+# A function to preprocess the tweets
+
+def text_preprocessing(text):
+    #Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings
+    # Convert words to lower case
+    text = text.lower()
+    # Expand contractions
+    if True:
+        text = text.split()
+        new_text = []
+        for word in text:
+            if word in contractions:
+                new_text.append(contractions[word])
+            else:
+                new_text.append(word)
+        text = " ".join(new_text)
+    # Format words and remove unwanted characters, removing mentions and hashtags
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'&amp;', '', text)
+    text = re.sub("@[A-Za-z0-9_]+","", text)
+    text = re.sub("#[A-Za-z0-9_]+","", text)
+    text = re.sub(r'[^\w\s]+', '', text)
+    text = re.sub(r'\ng?|\ngt?', ' ', text)
+    
+    # Tokenize each word
+    text = nltk.WordPunctTokenizer().tokenize(text)
+    # Lemmatize each word
+    text = [nltk.stem.WordNetLemmatizer().lemmatize(token, pos='v') for token in text if len(token)>1]
+    return text
+def to_string(text):
+    # Convert list to string
+    text = ' '.join(map(str, text))
+    return text
+
+# Add a new column that contains a list of tokens
+df_lgbt['clean_tweet_list'] = list(map(text_preprocessing, df_lgbt.text))
+# Add a new column 
+df_lgbt['clean_tweet'] = list(map(to_string, df_lgbt['clean_tweet_list']))
+
+# Removing stopwords using the stopwords package from nltk
+stopwords_list = stopwords.words('english')
+
+# Extending stopwords identified through iterative process
+stopwords_list.extend(['lgbt', 'lgbtq','lgbtq+','transgender' 'bisexual', 'gay', 'trans', 'queer', 'lesbian', 'soccer', 'football',
+                       'world', 'cup', 'qatar', 'fifa', 'fifaworldcup', '2022', 'sbsworldcup', 'qatars', 'worldcup', 'bi', 'gays', 'qataris',
+                      'qatari'])
+
+# Removing the stopwords from the list of tokens and cleaned tweets.
+df_lgbt['clean_tweet_list'] = [[word for word in line if word not in stopwords_list] for line in df_lgbt['clean_tweet_list']]
+df_lgbt['clean_tweet'] = list(map(to_string, df_lgbt['clean_tweet_list']))
+
+#Check new dataframe
+df_lgbt
+
+# Saving as CSV
+df.to_csv('final_clean_tweets_lgbt.csv', index=False)
+
+# Create Dictionary
+id2word = gensim.corpora.Dictionary(df_lgbt['clean_tweet_list'])
+
+# Create Corpus: Term Document Frequency + bag of words
+corpus = [id2word.doc2bow(text) for text in df_lgbt['clean_tweet_list']]
+
+
+from gensim.models import CoherenceModel
+# Compute Coherence Score
+number_of_topics = []
+coherence_score = []
+for i in range(1,5):
+  lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           iterations=50,
+                                           num_topics=i)
+  coherence_model_lda = CoherenceModel(model=lda_model, texts=df_lgbt['clean_tweet_list'], dictionary=id2word, coherence='c_v')
+  coherence_lda = coherence_model_lda.get_coherence()
+  number_of_topics.append(i) #append to the respective list
+  coherence_score.append(coherence_lda);
+
+# Returning a list of tuples 
+for idx, topic in lda_model.print_topics(-1):
+    print("Topic: {} Word: {}".format(idx, topic))
+    print("\n")
+
+# Creating the topic modelling visualisation
+import pyLDAvis.gensim_models
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary=lda_model.id2word)
+vis
+
+# Checking tweet sample in topics 
+data_dict = {'dominant_topic':[], 'perc_contribution':[], 'topic_keywords':[]}
+
+# Assigning the dominant topic, its contribution, and keywords to a dictionary 
+for i, row in enumerate(lda_model[corpus]):
+    #print(i)
+    row = sorted(row, key=lambda x: x[1], reverse=True)
+    #print(row)
+    for j, (topic_num, prop_topic) in enumerate(row):
+        wp = lda_model.show_topic(topic_num)
+        topic_keywords = ", ".join([word for word, prop in wp])
+        data_dict['dominant_topic'].append(int(topic_num))
+        data_dict['perc_contribution'].append(round(prop_topic, 3))
+        data_dict['topic_keywords'].append(topic_keywords)
+        #print(topic_keywords)
+        break
+
+df_topics = pd.DataFrame(data_dict)
+
+contents = pd.Series(tweets)
+
+df_topics['text'] = df_lgbt['text']
+df_topics
+
+# Repeat above code with the only world cup dataset (world_cup_no_lgbt_tweets.csv)
+
 # CREATING A STYLECLOUD
+
 # Creating a new DataFrame with only the required column
 clean_df = pd.DataFrame(df_lgbt['clean_tweet'])
 
@@ -178,3 +396,5 @@ from IPython.display import Image
 Image(filename='stylecloud_lgbt.png')
 
 # Repeated the above code to create a second stylecloud only including the worldcup dataset
+
+
